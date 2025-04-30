581789b483189f810a71d5fb29a8c48a2ffd23a4
aditya45210l <aditya45210l@gmail.com>
2025-04-30 14:11:07 +0530

    Write Deploy Script for Foundry-Nft-F25 and Write some awsome Test

--- foundry-NFT-f25/script/DeployMoodNft.s.sol
+++ foundry-NFT-f25/script/DeployMoodNft.s.sol
@@ -1,15 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/Script.sol";
+import {Script,console} from "forge-std/Script.sol";
 import {MoodNft} from "../src/MoodNft.sol";
-import {Utils} from "../test/Utils/Utils.sol";
+import {Base64}  from "@openzeppelin/contracts/utils/Base64.sol";
+// import {happy} from "../images/happy.svg";
 
-contract DeployMoodNft is Script, Utils {
-    function run() external returns (MoodNft) {
+contract DeployMoodNft is Script {
+    MoodNft moodNft;
+    function run() external returns(MoodNft) {
+        string memory happySvg = vm.readFile("./images/happy.svg");
+        string memory sadSvg = vm.readFile("./images/sad.svg");
+
         vm.startBroadcast();
-        MoodNft moodNft = new MoodNft(HAPPY_URI, SAD_SVG);
+        moodNft = new MoodNft(svgToImageURI(happySvg),svgToImageURI(sadSvg));
         vm.stopBroadcast();
+
         return moodNft;
+    }
+
+
+    function svgToImageURI(string memory svg) public pure returns(string memory ) {
+        string memory baseUrl = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(baseUrl,svgBase64Encoded));
     }
 }
